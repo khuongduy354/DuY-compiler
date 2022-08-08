d43029429299d180582d12c1063858d9a1f1f336
khuongduy354 <khuongduy354@gmail.com>
2022-08-08 20:51:19 +0700

    test(tokenizer):single,double,literals

--- src/test.rs
+++ src/test.rs
@@ -1,70 +1,63 @@
-// //variables, operators, if, loop, function
-
-// enum Value {
-//     String(String),
-//     Number(f64),
-//     Boolean(bool),
-// }
-
-// enum Scope {
-//     Function,
-//     Main,
-// }
-
-// struct Variable {
-//     value: Value,
-//     name: String,
-//     scope: Scope,
-// }
-
-// pub struct Function {
-//     name: String,
-//     args: Vec<Variable>,
-//     body: Vec<Statement>,
-// }
-
-// // pub struct If{
-// // condition:
-// // }
-
-// pub enum Statement {
-//     Assignment,
-//     If,
-//     Loop,
-//     FunctionCall(Function),
-// }
-
-// pub enum Operator {
-//     Add,
-//     Sub,
-//     Mul,
-//     Div,
-//     Mod,
-//     Pow,
-//     Eq,
-//     Neq,
-//     And,
-//     Or,
-// }
-// pub enum Expression {
-//     Unary {
-//         operator: Operator,
-//         value: Value,
-//     },
-//     Binary {
-//         operator: Operator,
-//         lhs: Value,
-//         rhs: Value,
-//     },
-// }
-
 #[cfg(test)]
 mod test {
-    use crate::{tokenizer::Tokenizer, types::Token};
 
-    pub fn basic_token() {
-        let test_str = String::from("function myname");
-        let mut tokenizer = Tokenizer::new(&test_str);
-        let result_vec = tokenizer.tokenize_full_src().unwrap();
+    use crate::{
+        tokenizer::{self, Tokenizer},
+        types::Token,
+    };
+
+    #[test]
+    pub fn single_token() {
+        let test_inp = vec!["+", "-", "*", ";", "^", "/", "="];
+        for operator in test_inp {
+            let operator = operator.to_string();
+            let mut tokenizer = Tokenizer::new(&operator);
+            let result = tokenizer
+                .lex_next_token()
+                .expect("Cannot parse single token");
+            // assert!(matches!(result, Token));
+        }
+    }
+    #[test]
+    pub fn double_tokens() {
+        let test_inp = vec!["<", ">", ">=", "<=", ":="];
+
+        for operator in test_inp {
+            let operator = operator.to_string();
+            let mut tokenizer = Tokenizer::new(&operator);
+
+            let result = tokenizer
+                .lex_next_token()
+                .expect("Cannot parse single token");
+        }
+    }
+
+    #[test]
+    pub fn literals() {
+        let test_inp = vec!["'this is a string'", "124124", "12.4124"];
+
+        let mut tokenizer = Tokenizer::new(test_inp[0]);
+        if let Token::StringLiteral(x) = tokenizer.lex_next_token().unwrap() {
+            assert_eq!(x, "this is a string");
+            // println!("{}", x);
+        } else {
+            panic!("Cannot tokenize string literal")
+        };
+
+        let mut tokenizer = Tokenizer::new(test_inp[1]);
+        if let Token::IntegerLiteral(x) = tokenizer.lex_next_token().unwrap() {
+            assert_eq!(x, 124124);
+            // println!("{}", x);
+        } else {
+            panic!("Cannot tokenize integer literal")
+        };
+
+        let mut tokenizer = Tokenizer::new(test_inp[2]);
+        if let Token::FloatLiteral(x) = tokenizer.lex_next_token().unwrap() {
+            assert_eq!(x, 12.4124);
+            // println!("{}", x);
+        } else {
+            panic!("Cannot tokenize float literal")
+        };
     }
 }
