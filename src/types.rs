4614c605c660a3916c2496b701c5d7e4a063c20f
khuongduy354 <khuongduy354@gmail.com>
2022-08-09 21:09:41 +0700

    feat(types):add a few token enums, clone trait for token

--- src/types.rs
+++ src/types.rs
@@ -27,6 +27,9 @@ pub enum Token {
     Comment,   // // or {}
     EOF,
     WhiteSpace,
+    OParen,
+    CParen,
+    //TODO: add array square brackets
 
     // operators
     Plus,    // +
@@ -39,6 +42,18 @@ pub enum Token {
     And,     // and
     Or,      // or
     Not,     // not
+    Plus,   // +
+    Minus,  // -
+    Mul,    // *
+    Assign, // :=
+    Div,    // div
+    Mod,    // mod
+
+    Pow, // ^
+
+    And, // and
+    Or,  // or
+    Not, // not
     Neq,     // <>
     Eq,      // =
     Less,    // <
@@ -60,6 +75,7 @@ pub enum Token {
     StringLiteral(String), //string value
     IntegerLiteral(i64),   //number value
     FloatLiteral(f64),     //number value
+    BooleanLiteral(bool),
 }
 
 impl Token {
@@ -67,6 +83,57 @@ impl Token {
         match self {
             Token::WhiteSpace => true,
             _ => false,
+        }
+    }
+}
+impl Clone for Token {
+    fn clone(&self) -> Token {
+        match self {
+            Token::Identifier(s) => Token::Identifier(s.to_string()),
+            Token::StringLiteral(s) => Token::StringLiteral(s.to_string()),
+            Token::IntegerLiteral(i) => Token::IntegerLiteral(*i),
+            Token::FloatLiteral(f) => Token::FloatLiteral(*f),
+            Token::BooleanLiteral(b) => Token::BooleanLiteral(*b),
+            //account for all types of tokens
+            Token::OParen => Token::OParen,
+            Token::CParen => Token::CParen,
+            Token::Abs => Token::Abs,
+            Token::Sort => Token::Sort,
+            Token::Len => Token::Len,
+            Token::Endl => Token::Endl,
+            Token::Write => Token::Write,
+            Token::Read => Token::Read,
+            Token::Sqrt => Token::Sqrt,
+            Token::Var => Token::Var,
+            Token::If => Token::If,
+            Token::Then => Token::Then,
+            Token::Else => Token::Else,
+            Token::While => Token::While,
+            Token::For => Token::For,
+            Token::Do => Token::Do,
+            Token::Function => Token::Function,
+            Token::Begin => Token::Begin,
+            Token::End => Token::End,
+            Token::Plus => Token::Plus,
+            Token::Minus => Token::Minus,
+            Token::Mul => Token::Mul,
+            Token::Assign => Token::Assign,
+            Token::Div => Token::Div,
+            Token::Mod => Token::Mod,
+            Token::Pow => Token::Pow,
+            Token::And => Token::And,
+            Token::Or => Token::Or,
+            Token::Not => Token::Not,
+            Token::Neq => Token::Neq,
+            Token::Eq => Token::Eq,
+            Token::Less => Token::Less,
+            Token::LessEq => Token::LessEq,
+            Token::Great => Token::Great,
+            Token::GreatEq => Token::GreatEq,
+            Token::EOF => Token::EOF,
+            Token::SemiColon => Token::SemiColon,
+            Token::Comment => Token::Comment,
+            Token::WhiteSpace => Token::WhiteSpace,
         }
     }
 }
